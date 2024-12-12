73f207d1a958c1cfaedd8313d4ebcb74c2573aaf
beeb <703631+beeb@users.noreply.github.com>
2024-12-12 17:19:00 +0100

    docs: natspec

--- src/interfaces/IUsdnLongStaking.sol
+++ src/interfaces/IUsdnLongStaking.sol
@@ -7,6 +7,6 @@ import { IUsdnLongStakingTypes } from "./IUsdnLongStakingTypes.sol";
 
 /**
  * @title IUsdnLongStaking
- * @notice Interface for the USDN Long Staking
+ * @notice Interface for the USDN Long Staking.
  */
 interface IUsdnLongStaking is IUsdnLongStakingTypes, IUsdnLongStakingErrors, IUsdnLongStakingEvents { }
