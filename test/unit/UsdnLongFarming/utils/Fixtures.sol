9068366b7fb50efc6a5c858c61e0bbc88af94612
samooyo <samidarnaud@gmail.com>
2025-01-03 11:48:02 +0100

    fix(dependencies): rename the namespace of dependencies

--- test/unit/UsdnLongFarming/utils/Fixtures.sol
+++ test/unit/UsdnLongFarming/utils/Fixtures.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
 
-import { LibRLP } from "solady-0.0/utils/LibRLP.sol";
+import { LibRLP } from "solady/src/utils/LibRLP.sol";
 
 import { DEPLOYER } from "../../../utils/Constants.sol";
 import { BaseFixture } from "../../../utils/Fixtures.sol";
