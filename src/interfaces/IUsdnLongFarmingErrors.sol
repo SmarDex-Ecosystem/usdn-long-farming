2628977183f595edc20bd2581137d29761dd58fe
samooyo <samidarnaud@gmail.com>
2025-01-06 11:39:48 +0100

    docs: errors natspecs

--- src/interfaces/IUsdnLongFarmingErrors.sol
+++ src/interfaces/IUsdnLongFarmingErrors.sol
@@ -1,19 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+/**
+ * @title USDN Long Farming Errors
+ * @dev Al errors used in the USDN Long Farming contract.
+ */
 interface IUsdnLongFarmingErrors {
-    /// @dev The USDN protocol position is owned by the contract.
+    /// @dev The USDN protocol position is already owned by the contract.
     error UsdnLongFarmingAlreadyDeposited();
 
     /// @dev The USDN protocol position validation is pending.
     error UsdnLongFarmingPendingPosition();
 
-    /// @dev The USDN protocol position is invalid.
+    /// @dev The USDN protocol position does not exist.
     error UsdnLongFarmingInvalidPosition();
 
-    /// @dev The specified notifierRewardsBps is invalid.
+    /// @dev The specified `notifierRewardsBps` value is invalid.
     error UsdnLongFarmingInvalidNotifierRewardsBps();
 
-    /// @dev The caller is invalid.
+    /// @dev The caller is not the owner of the USDN protocol position.
     error UsdnLongFarmingInvalidCaller();
 }
