80219aa4624107c2ad56f4c5966f2da8838222c4
fireboss777 <fireboss@tutanota.com>
2025-01-07 17:11:31 +0100

    test: transferPositionOwnership

--- test/integration/utils/Fixtures.sol
+++ test/integration/utils/Fixtures.sol
@@ -29,36 +29,40 @@ contract UsdnLongFarmingIntegrationFixture is
     FarmingToken internal farmingToken;
     IFarmingRange internal rewardsProvider = IFarmingRange(0x7d85C0905a6E1Ab5837a0b57cD94A419d3a77523);
     UsdnLongFarmingHandler internal farming;
+
     uint256 internal constant TOTAL_REWARDS = 100_000_000 ether;
     uint256 internal constant REWARDS_PER_BLOCK = 100;
 
+    uint256 internal campaignId;
+    uint256 internal startBlock;
     address internal farmingAddress;
     address internal farmingRangeOwner;
     address internal rewardManager;
-    uint256 internal campaignId;
-    uint256 internal startBlock;
 
     function _setUp() internal virtual {
+        string memory url = vm.rpcUrl("mainnet");
+        vm.createSelectFork(url);
+        vm.rollFork(20_014_134);
+
         // deploy usdn protocol
         params = DEFAULT_PARAMS;
-        params.initialDeposit = 202 ether;
-        params.initialLong = 200 ether;
-        params.fork = true;
+        params.initialDeposit = 502 ether;
+        params.initialLong = 500 ether;
         _setUp(params);
 
         // deploy farming token
         vm.prank(DEPLOYER);
         farmingToken = new FarmingToken();
 
+        // store values
         farmingAddress = LibRLP.computeAddress(DEPLOYER, vm.getNonce(DEPLOYER));
         farmingRangeOwner = rewardsProvider.owner();
         rewardManager = rewardsProvider.rewardManager();
         campaignId = rewardsProvider.campaignInfoLen();
         startBlock = block.number + 10;
 
+        // set smardex farming rewards
         deal(address(rewardToken), rewardManager, TOTAL_REWARDS);
-
-        // set farming campaign and reward infos
         vm.startPrank(farmingRangeOwner);
         rewardsProvider.addCampaignInfo(farmingToken, rewardToken, startBlock);
         rewardsProvider.addRewardInfo(campaignId, startBlock + TOTAL_REWARDS / REWARDS_PER_BLOCK, REWARDS_PER_BLOCK);
