ff838074c3da14c5159760a22945e9e6f228d72d
fireboss777 <fireboss@tutanota.com>
2025-01-22 17:00:50 +0100

    chore: remove function

--- script/02_DeployUsdnLongFarming.s.sol
+++ script/02_DeployUsdnLongFarming.s.sol
@@ -16,8 +16,6 @@ contract DeployUsdnLongFarming is Script {
     IERC20 internal FARMING_TOKEN = IERC20(0xCE1bc72A070349cb444743Ec3b2b4d8BF398DAf5);
 
     function run() external returns (UsdnLongFarming longFarming_) {
-        _handleEnvVariables();
-
         uint256 campaignID = FARMING_RANGE.campaignInfoLen() - 1;
         while (campaignID >= 0) {
             IFarmingRange.CampaignInfo memory campaignInfo = FARMING_RANGE.campaignInfo(campaignID);
@@ -35,11 +33,5 @@ contract DeployUsdnLongFarming is Script {
         FARMING_TOKEN.approve(longFarmingAddress, 1);
         longFarming_ = new UsdnLongFarming(USDN_PROTOCOL, FARMING_RANGE, campaignID);
         vm.stopBroadcast();
-    }
-
-    /// @notice Handle the environment variables
-    function _handleEnvVariables() internal {
-        string memory etherscanApiKey = vm.envOr("ETHERSCAN_API_KEY", string("XXXXXXXXXXXXXXXXX"));
-        vm.setEnv("ETHERSCAN_API_KEY", etherscanApiKey);
     }
 }
